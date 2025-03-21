07710de45f959b3cf19f900a1d88e74238d1907a
NammaSportika <nammasportika@gmail.com>
2025-03-21 11:20:05 +0530

    V4

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -1,7 +1,22 @@
-with open('ThirdVersionData.txt', 'r') as file:
-    a = float(file.readline())
-    b = float(file.readline())
-    c = float(file.readline())
-    x = float(file.readline())
-y = a*x*x+b*x+c
-print('The value of y is:', y)
+import matplotlib.pyplot as plt
+x_values = []
+y_values = []
+with open('FourthVersionData.txt', 'r') as file:
+    for _ in range(40):
+        try:
+            a = float(file.readline().split('=')[1])
+            b = float(file.readline().split('=')[1])
+            c = float(file.readline().split('=')[1])
+            x = float(file.readline().split('=')[1])
+            y = a*x*x+b*x+c
+            x_values.append(x)
+            y_values.append(y)
+        except (IndexError, ValueError):
+            print("Error reading a line. Check your data format.")
+            break
+plt.plot(x_values, y_values, marker='o', linestyle='-', color='b')
+plt.title('Graph of y = ax^2 + bx + c')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.grid(True)
+plt.show()
